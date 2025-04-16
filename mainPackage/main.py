3f9d90365f8db8c51a8c5e41a92568df9b676a81
oalkhawaga <oalkhawaga@outlook.com>
2025-04-16 17:54:02 -0400

    Omars changes- updated main and function.py

--- mainPackage/main.py
+++ mainPackage/main.py
@@ -1,8 +1,18 @@
-# main.py
+# File Name : main.py
+# Student Name: Omar Alkhawaga,Kengo
+# email: alkhawoe@mail.uc.edu
+# Assignment Number: Assignment 11
+# Due Date:  4/17/2025
+# Course #/Section:IS4010-001
+# Semester/Year:  Spring 2025
+# Brief Description of the assignment: We were given a CSV file and had to cleanup the file by doing some tasks
+# Brief Description of what this module does: 
+# Citations: https://chatgpt.com/, https://www.geeksforgeeks.org/working-csv-files-python/
+# Anything else that's relevant:N/A
 
 from requests.auth import parse_dict_header
 from functionPackage.function import *
-import pandas as pd
+
 
 
 
@@ -27,4 +37,20 @@ if __name__ == "__main__":
     
     # Delete data that has pepsi as Fuel Type from dataframe
     df = csv.delete_rows(df,'Fuel Type', 'Pepsi')
+
+    df = csv.split_address(df)  
+    df = csv.fill_missing_zip_codes(df)  
+   
+     # Fill missing ZIP codes
+    df = csv.fill_missing_zip_codes(df, city_column='City', zip_column='ZipCode')
+
+    
+
+    # Export final cleaned data
+    csv.export_to_csv(df, './Data/cleanedData.csv')
+    df = pd.read_csv(file_path, low_memory=False)
+
+    
+    
+    
     
